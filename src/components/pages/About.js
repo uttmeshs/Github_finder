4c8c35ea86eab30a64ffa24074549644eb92b481
Uttmesh Shukla <uttmeshs@gmail.com>
2020-09-17 12:39:57 +0530

    first major commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1> About this page</h1>
+      <p>This is a APP to search the GitHub users</p>
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
