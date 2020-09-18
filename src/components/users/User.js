cc11fe7b7fe51bc21a4841617429c2d8a69244cc
Uttmesh Shukla <uttmeshs@gmail.com>
2020-09-18 20:23:42 +0530

    updated user,js
    i
    :#

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,8 +1,47 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
+
+  static proppTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
   render() {
-    return <div></div>;
+    const {
+      name,
+      avator_url,
+      bio,
+      blog,
+      html_url,
+      followers,
+      following,
+      login,
+      hireable,
+    } = this.props.user;
+
+    const { loading } = this.props;
+
+    if (loading) return Spinner;
+    return (
+      <Fragment>
+        <Link to='/' className='btn btn-light'>
+          Back to Search
+        </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+      </Fragment>
+    );
   }
 }
 
