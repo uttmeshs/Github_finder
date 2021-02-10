d5ac02fb0721db2c01a07c340084905dc06727a8
Uttmesh Shukla <uttmeshs@gmail.com>
2021-02-10 10:51:42 +0530

    adding user functionality

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -17,6 +17,7 @@ export class User extends Component {
     const {
       name,
       avatar_url,
+      company,
       bio,
       blog,
       html_url,
@@ -24,6 +25,8 @@ export class User extends Component {
       following,
       login,
       location,
+      public_repos,
+      public_gists,
       hireable,
     } = this.props.user;
 
@@ -59,7 +62,37 @@ export class User extends Component {
                 <p>{bio}</p>
               </Fragment>
             )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && <Fragment>
+                  <strong>Username: </strong> {login}
+                  </Fragment>}
+              </li>
+
+              <li>
+                {company && <Fragment>
+                  <strong>Company: </strong> {company}
+                  </Fragment>}
+              </li>
+
+              <li>
+                {blog && <Fragment>
+                  <strong>Website: </strong> {blog}
+                  </Fragment>}
+              </li>
+
+             
+            </ul>
           </div>
+        </div>
+        <div className='card text-center'>
+          <div className='badge badge-primary'>Followers: {followers}</div>
+          <div className='badge badge-success'>Following: {following}</div>
+          <div className='badge badge-danger'>Public Repos: {public_repos}</div>
+          <div className='badge badge-dark'>Public Gists: {public_gists}</div>
         </div>
       </Fragment>
     );
